c2d3cbbf203144351bba165cf7c7a4170cb044bc
Ludovic <villeg300@gmail.com>
2025-09-25 23:43:19 +0000

    btn mailto

--- static/js/contact.js
+++ static/js/contact.js
@@ -2,7 +2,6 @@ const form = document.getElementById('contact_form')
 
 form.addEventListener('submit',
     async (e)=>{
-        e.preventDefault()
         const formData = new FormData(form)
         const data = Object.fromEntries(formData.entries())
     }
